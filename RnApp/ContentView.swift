08d529156ddd19820f3a8b180080fcaa475c7021
Landonhollis <landonhollis2006@gmail.com>
2025-02-05 17:25:09 -0500

    delet
    delete

--- RnApp/ContentView.swift
+++ RnApp/ContentView.swift
@@ -5,20 +5,5 @@
 //  Created by Landon Hollis on 2/5/25.
 //
 
-import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-#Preview {
-    ContentView()
-}
